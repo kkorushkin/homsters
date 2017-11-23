acc166cafb00ff2ee0bd5da90c1c2d363400f262
Kirill Korushkin <kkorushkin@gmail.com>
2017-11-23 02:04:54 +0200

    commit

--- config.php
+++ config.php
@@ -1,2 +1,6 @@
 <?php
 define('DIR',$_SERVER['DOCUMENT_ROOT']);
+
+define('TITLE','Продажа новостроя в Казахстане | homsters.kz');
+define('META_DESCRIPTION','Продажа новостроя в Казахстане | homsters.kz');
+define('META_KEYWORDS','Продажа новостроя в Казахстане | homsters.kz');
--- index.php
+++ index.php
@@ -1,39 +1,19 @@
+<?php include 'config.php';?>
 <!DOCTYPE html>
 <html>
 <head lang="ru">
     <meta charset="UTF-8">
-    <title>Продажа новостроя в Казахстане | homsters.kz</title>
+    <title><?=TITLE;?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="keywords" content="">
+    <meta name="description" content="<?=META_DESCRIPTION;?>">
+    <meta name="keywords" content="<?=META_KEYWORDS;?>">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
     <script src="js/jquery.min.js"></script>
 </head>
 <body>
 <header class="container">
-    <div id="header-top" class="row">
-        <nav class="navbar">
-                <div class="container-fluid">
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <img src="images/homsters-kz-logo.svg" class="navbar-brand" alt="homsters.kz"/>
-                    </div>
-                    <div class="collapse navbar-collapse" id="myNavbar">
-                        <ul class="nav navbar-nav">
-                            <li><a href="#testimanials-one" data-to="testimanials-one">Что говорят застройщики</a></li>
-                            <li><a href="#advantage-one" data-to="advantage-one">Цифры о нас</a></li>
-                            <li><a href="#">Видео</a></li>
-                            <li><div id="menu-cta" data-toggle="modal" data-target="#сall-me">Подключтить мой ЖК бесплатно</div></li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-    </div>
+    <?php include 'includes/header.html';?>
 </header>
 <div class="container">
     <div id="video-banner" class="row">
